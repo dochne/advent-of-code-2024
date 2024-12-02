fc0d08d47cb74b6c0fd692d78b35caa4b98e319c
Doug Nelson <doug@dochne.com>
2024-12-02 13:50:47 +0000

    update day01 to be prettier

--- day01/part1.rb
+++ day01/part1.rb
@@ -1,20 +1,12 @@
 #!/usr/bin/env ruby
 
-input = STDIN.read.lines(chomp: true)
-  .map{_1.split(" ")}
-  .each_with_object([[], []]).each do |value, acc|
-    acc[0] << value[0].to_i
-    acc[1] << value[1].to_i
-  end
-  .yield_self {|value| value.map{_1.sort}}
-  .yield_self do |value|
-    response = []
-    value[0].each_with_index do |n, i|
-      response << [n, value[1][i]].max - [n, value[1][i]].min
-    end
-    response
-  end
+left, right = STDIN.read.lines(chomp: true)
+  .map{_1.split(" ").map(&:to_i)}
+  .transpose
+  .map(&:sort)
+
+output = left.each_index
+  .map {|i| [left[i], right[i]].max - [left[i], right[i]].min}
   .sum
 
-p(input)
-# p(input)
+p(output)
--- day01/part2.rb
+++ day01/part2.rb
@@ -1,15 +1,13 @@
 #!/usr/bin/env ruby
 
-input = STDIN.read.lines(chomp: true)
-  .map{_1.split(" ")}
-  .each_with_object([[], []]).each do |value, acc|
-    acc[0] << value[0].to_i
-    acc[1] << value[1].to_i
-  end
-  .yield_self do |(left, right)|
-    multipliers = right.tally
-    left.map{|n| n *  (multipliers[n] || 0)}
-  end
+left, right = STDIN.read.lines(chomp: true)
+  .map{_1.split(" ").map(&:to_i)}
+  .transpose
+
+tally = right.tally  
+
+output = left
+  .map{|n| n * (tally[n] || 0)}
   .sum
 
-p(input)
+p(output)  
