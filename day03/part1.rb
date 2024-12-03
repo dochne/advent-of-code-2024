31ab7d72b12d22b560b7c79a1a658ef997c2949a
Doug Nelson <doug@dochne.com>
2024-12-03 09:26:40 +0000

    in this house, we use reduce

--- day03/part1.rb
+++ day03/part1.rb
@@ -3,6 +3,6 @@
 STDIN.read.lines(chomp: true)
     .join("")
     .scan(/mul\((\d{1,3}),(\d{1,3})\)/)
-    .reduce(0) {|acc, row| acc + row.map(&:to_i).inject(:*)}
+    .reduce(0) {|acc, row| acc + row.map(&:to_i).reduce(:*)}
     .tap{p(_1)}
     
