dba4520df58f8b10bf30b92d161cb85e976264fc
Doug Nelson <doug@dochne.com>
2024-12-03 10:33:00 +0000

    no brackets needed

--- day03/part2.rb
+++ day03/part2.rb
@@ -7,7 +7,7 @@ STDIN.read.lines(chomp: true)
         case row[0]
         when "do" then acc[1] = 1
         when "don't" then acc[1] = 0
-        when "mul" then acc[0] += acc[1] * (row[1].to_i * row[2].to_i)
+        when "mul" then acc[0] += acc[1] * row[1].to_i * row[2].to_i
         end
     end
     .tap{p(_1[0])}
