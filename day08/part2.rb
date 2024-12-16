e6e24ae8bb1332a68ce7f1d80c46e38984a5aab5
Doug Nelson <doug@dochne.com>
2024-12-16 18:06:50 +0000

    even more terse

--- day08/part2.rb
+++ day08/part2.rb
@@ -3,7 +3,7 @@
 require "matrix"
 require "set"
 
-State = Struct.new(:node_map, :width, :height, :anti_nodes) do
+State = Struct.new(:node_map, :width, :height) do
     def is_valid?(vector) vector[0].between?(0, width - 1) && vector[1].between?(0, height - 1) end
 end
 
@@ -11,17 +11,19 @@ STDIN.read.lines(chomp: true)
     .each_with_index
     .yield_self do |grid|
         State.new(
-            grid.each_with_object([]) do |(cell, row_idx), acc|
+            grid.each_with_object(Hash.new{|h, k| h[k] = []}) do |(cell, row_idx), acc|
                 cell.split("")
                     .each_with_index
-                    .each_with_object(acc) {|(value, col_idx), acc| acc << Vector[col_idx, row_idx] if value != "." }
+                    .each_with_object(acc) do |(value, col_idx), acc|
+                        acc[value] << Vector[col_idx, row_idx] if value != "."
+                    end
             end,
             grid.first[0].length,
             grid.to_a.length
         )
     end
-    .tap do |state|
-        state.anti_nodes = state.node_map.reduce(Set.new) do |acc, (cell, nodes)|
+    .yield_self do |state|
+        state.node_map.reduce(Set.new) do |acc, (cell, nodes)|
             nodes.combination(2).each_with_object(acc) do |(a, b), acc|
                 freq = a - b
                 acc << a while state.is_valid?(a += freq)
@@ -29,5 +31,5 @@ STDIN.read.lines(chomp: true)
             end + nodes
         end
     end
-    .yield_self { _1.anti_nodes.length }
+    .yield_self { _1.length }
     .tap{ p(_1) }
